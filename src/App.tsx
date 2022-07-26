6102d26cff1fce2c2ccf64878d00d869b54541cd
Evil-Developer <pro.expert0525@gmail.com>
2022-07-26 18:40:48 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import MainRoute from './routes/MainRoute';
+
+function App() {
+  return <MainRoute></MainRoute>;
+}
+
+export default App;
---
+++ src/pages/MainPage/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+const MainPage = () => {
+  return <div className={styles.wrapper}>Company birthday guests</div>;
+};
+
+export default MainPage;
