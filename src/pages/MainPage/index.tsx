a42dd7137e2fd69bf6a16619f3ec48c5701877ff
Evil-Developer <pro.expert0525@gmail.com>
2022-07-29 03:41:12 +0300

    Fix: change getPartners function, change partnerType, convert partner_id to id in partners.json file.

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -5,51 +5,43 @@ import PartnerTable from '../../components/PartnerTable';
 
 import styles from './styles.module.scss';
 
+const deg2rad = (deg: number) => {
+  return deg * (Math.PI / 180);
+};
+const earthRadius = 6317.009;
+
 const MainPage = () => {
   const [selectedPartners, setSelectedPartners] = useState<Array<PartnerType>>([]);
 
-  const deg2rad = (deg: number) => {
-    return deg * (Math.PI / 180);
-  };
+  const pow = Math.pow;
+  const sin = Math.sin;
+  const companyLng = deg2rad(23.351723);
+  const companyLat = deg2rad(42.6665921);
 
   const getDistance = (longitude: number, latitude: number) => {
-    const pow = Math.pow;
-    const sin = Math.sin;
-    const cLongitude = deg2rad(23.351723);
-    const cLatitude = deg2rad(42.6665921);
-    const dLongitude = cLongitude - longitude;
-    const dLatitude = cLatitude - latitude;
+    const deltaLng = companyLng - longitude;
+    const deltaLat = companyLat - latitude;
 
     return (
-      6317.009 *
+      earthRadius *
       (2 *
         Math.asin(
           Math.sqrt(
-            pow(sin(dLatitude / 2), 2) +
-              (1 - pow(sin(dLatitude / 2), 2) - pow(sin((cLatitude + latitude) / 2), 2)) * pow(sin(dLongitude / 2), 2)
+            pow(sin(deltaLat / 2), 2) +
+              (1 - pow(sin(deltaLat / 2), 2) - pow(sin((companyLat + latitude) / 2), 2)) * pow(sin(deltaLng / 2), 2)
           )
         ))
     );
   };
 
   const getPartners = () => {
-    const temp: Array<PartnerType> = [];
-    partners
-      .sort((a, b) => a.partner_id - b.partner_id)
-      .map(partner => {
-        const distance = getDistance(deg2rad(parseFloat(partner.longitude)), deg2rad(parseFloat(partner.latitude)));
-        {
-          distance <= 100 &&
-            temp.push({
-              id: partner.partner_id,
-              name: partner.name,
-              longitude: partner.longitude,
-              latitude: partner.latitude,
-              distance: distance,
-            });
-        }
-      });
-    setSelectedPartners(temp);
+    setSelectedPartners(
+      partners
+        .sort((a, b) => a.id - b.id)
+        .filter(
+          partner => getDistance(deg2rad(parseFloat(partner.longitude)), deg2rad(parseFloat(partner.latitude))) <= 100
+        )
+    );
   };
 
   useEffect(() => {
--- src/shared/types.d.ts
+++ src/shared/types.d.ts
@@ -3,5 +3,4 @@ interface PartnerType {
   name: string;
   latitude: string;
   longitude: string;
-  distance: number;
 }
